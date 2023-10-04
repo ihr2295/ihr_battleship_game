4175a3263b68108c541f5a342d323399f9922710
ihr2295 <134147685+ihr2295@users.noreply.github.com>
2023-10-04 12:35:13 +0200

    Update run.py and adding tie function

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ class Player:
         self.board = [['.'for _ in range(boardsize)] for _ in range(boardsize)]
         self.ships = []
         self.guesses = []
+        self.score = 0
 
     def place_ships(self):
         for _ in range(self.num_ships):
@@ -33,7 +34,7 @@ class Player:
                 int(guess) < 0 or
                     int(guess) >= self.boardsize):
                 print(
-                    f"Wrong.Enter between 0 and {self.boardsize - 1}")
+                    f"Wrong. Enter between 0 and {self.boardsize - 1}")
                 continue
 
             row = int(guess)
@@ -50,12 +51,13 @@ class Player:
                 if (row, col) not in self.guesses:
                     self.guesses.append((row, col))
                     if (row, col) in opponent.ships:
-                        print(f"Congratulations, {self.name}! You hit a ship!")
-                        self.board[row][col] = 'X '
+                        print(f"{self.name} got a hit!")
+                        opponent.board[row][col] = 'X '  # Mark as 'X' for hits
+                        self.score += 1  # Assign a point to the player
                         return True
                     else:
-                        print("Oops! It's a miss.")
-                        self.board[row][col] = 'M '
+                        print(f"{self.name} missed.")
+                        opponent.board[row][col] = 'M '  # Update opponent bord
                         return False
                 else:
                     print("You've already guessed this coordinate. Try again.")
@@ -72,9 +74,9 @@ class Player:
                         if self.name == "Computer":
                             board_str += '. '  # Hide computer's ship locations
                         else:
-                            board_str += '@ '
+                            board_str += 'X '  # Display 'X' for hits
                     else:
-                        board_str += 'M '  # Miss
+                        board_str += 'M '  # Display 'M' for misses
                 else:
                     if self.name == "Computer":
                         board_str += '. '  # Hide computer's ship locations
@@ -95,6 +97,85 @@ def welcome_message():
     print("-----------------------------------")
 
 
+def play_game(player, computer):
+    player_score = 0
+    computer_score = 0
+    max_guesses = 10
+
+    while True:
+        if (len(player.guesses) >= max_guesses and
+                len(computer.guesses) >= max_guesses):
+            print("\nMax number of guesses reached. The game is a tie!")
+            break
+
+        if len(player.guesses) >= max_guesses:
+            print(f"\nMax number of guesses reached. {player.name} loses!")
+            break
+
+        if len(computer.guesses) >= max_guesses:
+            print("\nMax number of guesses reached. Computer loses!")
+            break
+
+        print("\nComputer's Board:")
+        # Display computer's board with player's guesses
+        print(computer.displayboard(player.guesses))
+
+        player_turn = player.make_guess(computer)
+
+        print(f"\n{player.name} guessed:", player.guesses[-1])
+        if player_turn:
+            print(f"{player.name} got a hit!")
+            player_score += 1  # Assign a point to the player
+
+        if player.all_ships_sunk():
+            print(
+                f"Congrats, {player.name}! You sank all the ships. You win!")
+            break
+
+        computer_turn = False
+        computer_guess = (0, 0)  # Initialize to avoid NameError
+        while not computer_turn:
+            computer_guess = (
+                random.randint(0, player.boardsize - 1),
+                random.randint(0, player.boardsize - 1),
+            )
+            if computer_guess not in computer.guesses:
+                computer.guesses.append(computer_guess)
+                if computer_guess in player.ships:
+                    print("Computer guessed:", computer_guess)
+                    print("Computer got a hit!")
+                    player.board[computer_guess[0]][computer_guess[1]] = 'X '
+                    computer_score += 1  # Assign a point to the computer
+                else:
+                    print("Computer guessed:", computer_guess)
+                    print("Computer missed this time.")
+                    player.board[computer_guess[0]][computer_guess[1]] = 'M '
+                computer_turn = True
+
+        print("-----------------------------------")
+        print("After this round, the scores are:")
+        print(f"{player.name}: {player_score}. Computer: {computer_score}")
+        print("-----------------------------------")
+
+        if computer.all_ships_sunk():
+            print("Computer sank all your ships. You lose!")
+            break
+
+    # Display the final state of the game boards
+    print("\nFinal State of the Game Boards:")
+    print(f"{player.name}'s Board:")
+    print(player.displayboard(player.guesses))
+    print("Computer's Board:")
+    print(computer.displayboard(computer.guesses))
+
+    if player_score > computer_score:
+        print(f"{player.name} wins!")
+    elif player_score < computer_score:
+        print(f"{player.name} loses!")
+    else:
+        print("The game is a tie!")
+
+
 def play_battleships():
     welcome_message()
 
@@ -107,52 +188,7 @@ def play_battleships():
     player.place_ships()
     computer.place_ships()
 
-    player_score = 0
-    computer_score = 0
-
-    while True:
-        print("\nComputer's Board:")
-        # Display computer's board with player's guesses
-        print(computer.displayboard(player.guesses))
-
-        player_turn = player.make_guess(computer)
-
-        print("\nPlayer guessed:", player.guesses[-1])
-        if player_turn:
-            print("Player got a hit!")
-
-        computer_turn = False
-        computer_guess = (0, 0)  # Initialize to avoid NameError
-        while not computer_turn:
-            computer_guess = (
-                random.randint(0, boardsize - 1),
-                random.randint(0, boardsize - 1),
-            )
-            if computer_guess not in computer.guesses:
-                computer.guesses.append(computer_guess)
-                if computer_guess in player.ships:
-                    print("Computer guessed:", computer_guess)
-                    print("Computer got a hit!")
-                    player.board[computer_guess[0]][computer_guess[1]] = 'X '
-                    player_score += 1
-                else:
-                    print("Computer guessed:", computer_guess)
-                    print("Computer missed this time.")
-                    player.board[computer_guess[0]][computer_guess[1]] = 'M '
-                computer_turn = True
-
-        print("-----------------------------------")
-        print("After this round, the scores are:")
-        print(f"{player.name}: {player_score}. Computer: {computer_score}")
-        print("-----------------------------------")
-
-        if player.all_ships_sunk():
-            print(
-                f"Congrats, {player.name}! You sank all the ships. You win!")
-            break
-        elif computer.all_ships_sunk():
-            print("Computer sank all your ships. You lose!")
-            break
+    play_game(player, computer)
 
 
 if __name__ == "__main__":
